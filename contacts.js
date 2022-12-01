6fbc62bb14cbc433b8f923ef6fdfb82c5b20621b
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-12-01 17:57:31 +0200

    add

--- contacts.js
+++ contacts.js
@@ -1,29 +1,60 @@
-const contactsPath = require("./db/contacts.json");
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
 
-// console.log(contactsPath);
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+const findId = "9";
 
-const fs = require("fs").promises;
+const newContact = {
+  name: "Eva Norton",
+  email: "Eva@mail.com",
+  phone: "(123) 45-67-89",
+};
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  fs.readFile("./db/contacts.json")
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err.message));
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      console.log(contacts);
+      return contacts;
+    })
+    .catch((e) => console.log(e.message));
 }
 
 function getContactById(contactId) {
-  // ...твой код
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contactFind = JSON.parse(data).find(
+        (contact) => contact.id === contactId
+      );
+      console.log(contactFind);
+      return contactFind;
+    })
+    .catch((e) => console.log(e.message));
 }
 
 function removeContact(contactId) {
   // ...твой код
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+function addContact(contactNew) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data);
+      contacts.push({ id: v4(), ...contactNew });
+      console.log(contacts);
+      fs.writeFile(contactsPath, JSON.stringify(contacts));
+      return contacts;
+    })
+    .catch((e) => console.log(e.message));
 }
 
 module.exports = {
-  contactsPath,
   listContacts,
 };
+
+// listContacts();
+// getContactById(findId);
+addContact(newContact);
+// addContact("./db/contacts.json", ["eva", "eva@gmail.com", `123456789`]);
